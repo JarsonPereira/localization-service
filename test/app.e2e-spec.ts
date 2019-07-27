97bb56d07ba2eea6661b1ebdd821713e68a7ad23
Jarson Oliveira <jarson2014@hotmail.com>
2019-07-27 05:07:51 -0300

    Adicionando exemplo de teste

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,8 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { RegisterStateInput } from 'src/localizationModule/commands/inputs/state/registerState.input';
+import { StateContract } from 'src/localizationModule/contracts/stateContract/state.contract';
 
-describe('AppController (e2e)', () => {
+describe('StateController (e2e)', () => {
   let app;
 
   beforeEach(async () => {
@@ -16,8 +18,24 @@ describe('AppController (e2e)', () => {
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .get('/api/states')
+      .expect(200);
+
   });
+
+  // exemplo de validação de classes
+  it(' Deve retonar estado inválido ', () => {
+
+    let input = new RegisterStateInput();
+    input.name = 'Tocantins';
+    // sigla vazia
+    input.initials = '';
+
+    let contract = new StateContract();
+    let inputValid = contract.validate(input);
+
+    return expect(false).toContain(inputValid);
+
+  });
+
 });
